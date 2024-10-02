85d1b8f65146dce57c950aa7b0a06714a83eae99
USER <USER@DESKTOP-JI95IOS>
2024-10-02 21:22:03 +0400

    Add project files.

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleApp21;
+using Dapper;
+using System.Data.SqlClient;
+
+var connectionString = "Server=DESKTOP-JI95IOS;Database=Dapper2;Trusted_Connection=True;Initial Catalog=Dapper2;Connect Timeout=30;Encrypt=False;";
+
+using var connection = new SqlConnection(connectionString);
+
+var sqlCommand = "INSERT INTO Teachers(Id,Name,GroupId) VALUES('1','Jane',1)";
+connection.ExecuteAsync(sqlCommand);
+
+var sqlCommand1 = "UPDATE Teachers SET Name = 'Jack' WHERE Id = 1";
+connection.Execute(sqlCommand1);
+
+var sqlCommand2 = "DELETE Teachers WHERE Id = 1";
+connection.Execute(sqlCommand2);
---
+++ ConsoleApp21/Teachers.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp21;
+
+public class Teachers
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public int GroupId { get; set; }
+
+    public override string ToString()
+    {
+        return $"Id {Id}\nName{Name}\nGroupId{GroupId}";
+    }
+
+}
